de776be2fd534c98457358d59ee42af6fb5b4b93
Akash <146610185+akashchoudhary4221@users.noreply.github.com>
2023-10-06 14:58:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+    clocks = new Date();
+    htime = clocks.getHours();
+    mtime = clocks.getMinutes();
+    stime = clocks.getSeconds();
+
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000);
